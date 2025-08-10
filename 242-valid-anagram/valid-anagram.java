cfb4c0922de9be484d40e15bce22facd7cb00f14
Jackson Lima <jacksonlima347@gmail.com>
2025-08-09 22:23:00 -0300

    Time: 4 ms (77.39%) | Memory: 44.9 MB (25.38%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,17 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
         char[] s1 = s.toCharArray();
         char[] t1 = t.toCharArray();
         
         Arrays.sort(s1);
         Arrays.sort(t1);
 
-        String s2 = new String(s1);
-        String t2 = new String(t1);
-
-        if (s2.equals(t2)){
-            return true;
-        }
-        return false;
+        return Arrays.equals(s1,t1);
     }
 }
