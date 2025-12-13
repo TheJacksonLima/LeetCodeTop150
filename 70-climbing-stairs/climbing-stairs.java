6895490cfbaf05338081538306e7ba2e3b2cacad
Jackson Lima <jacksonlima347@gmail.com>
2025-12-13 13:14:41 -0300

    Time: 0 ms (100.00%) | Memory: 42.1 MB (17.58%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int climbStairs(int n) {
+        /*
+        if (n ==0 || n==1){
+            return 1;
+        }
+
+        return climbStairs(n-1) + climbStairs(n-2);
+        */
+        if (n<=2) return n;
+        
+        int prev2 = 1;
+        int prev1 = 2;
+
+        for (int i=3;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
+}
