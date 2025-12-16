17e9cab31cb4a4dfd1dab9e93485dc295a94f4c7
Jackson Lima <jacksonlima347@gmail.com>
2025-12-16 11:11:26 -0300

    Time: 1 ms (97.22%) | Memory: 48.4 MB (20.37%) - LeetSync

---
+++ 203-remove-linked-list-elements/remove-linked-list-elements.kt
@@ -0,0 +1,31 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+class Solution {
+    fun removeElements(head: ListNode?, `val`: Int): ListNode? {
+        val dummy = ListNode(0)
+        dummy.next = head
+
+       var prev: ListNode? = dummy
+        var curr = head
+
+        while (curr != null){
+            if (curr.`val` == `val`){
+                prev?.next = curr.next
+            }
+            else{
+                prev = curr
+            }
+            curr = curr.next
+        }
+
+        return dummy.next
+        
+    }
+}
