8602baa911e17d2669e26cfbb8fd14bf2cbef028
Jackson Lima <jacksonlima347@gmail.com>
2026-01-13 15:58:34 -0300

    Time: 1 ms (93.60%) | Memory: 43.1 MB (8.72%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,48 +1,14 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        if ((s.length() > t.length()) || (t.length() == 0) && (s.length() > 0))
-        {
-            return false;
-        }
-
-        if ((s.length() == 0) && (t.length() > 0)) 
-        {
-            return true;
-        }
-
-        if ((s.length() == 0) && (t.length() == 0)){
-            return true;
-        }
-
-        if (s.length() == t.length()){
-            if (s == t)
-                return true;
-            else
-                return false;
-        }
-        
-
-
+        if (s == null || t == null) return false;
         int i=0;       
-        int contFound=0;       
-        char aux = s.charAt(i);
 
         for(char c : t.toCharArray()){
-            //System.out.println("c: "+c+ " aux:"+aux+ " i:"+i+" countFound: "+contFound);
-            if  (c == aux){
-                contFound+=1;
-                if ((s.length()-1) > i){ 
-                   i++;
-                }
-                aux = s.charAt(i);
+            if (i< s.length() && s.charAt(i) == c){
+                i++;
             }
         }
 
-        //System.out.println("i: "+i+" aux: "+aux+ " s.length(): "+s.length()+" contFound: "+contFound);
-        if (contFound == s.length()){
-            return true;
-        }
-
-        return false;
+        return i == s.length();
     }
 }
